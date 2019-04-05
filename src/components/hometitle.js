dbf99c179ddbeee187bc4227178327109da3fd01
Chris Maillefaud <christian@geru.com.br>
2019-04-05 16:24:07 -0300

    Dojo 05-abril

---
+++ src/components/hometitle.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const HomeTitle = () => (<h1>Pré-Cadastro</h1>);
+
+export default HomeTitle;
---
+++ src/components/messageok.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+class MessageOk extends React.Component {
+    render () {
+        return (<p>Enviado com sucesso!</p>)
+    }
+}
+
+export default MessageOk;
